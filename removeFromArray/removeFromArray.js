080674fe2776688f72b2fdb71f3c265781279b74
Chris Lormor <clormor@palantir.com>
2020-09-08 17:32:40 +0100

    add remove from array solution

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elementsToRemove) {
+    let result = array
+    let nextElementToRemove = elementsToRemove.shift()
+    while (nextElementToRemove) {
+        let nextOccurrence = result.findIndex(e => e === nextElementToRemove)
+        while (nextOccurrence !== -1) {
+            result.splice(nextOccurrence, 1)
+            nextOccurrence = result.findIndex(e => e === nextElementToRemove)
+        }
+        nextElementToRemove = elementsToRemove.shift()
+    }
+    return result
 }
 
 module.exports = removeFromArray
