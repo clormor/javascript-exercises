dd994aba52322c8cbb887621cfe86fb31b67f494
Chris Lormor <clormor@palantir.com>
2020-09-08 21:01:18 +0100

    add solution to sum all

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (!isNumber(x) || !isNumber(y)) {
+        return 'ERROR'
+    }
+    let max = parseInt(Math.max(x,y))
+    let min = parseInt(Math.min(x,y))
+    if (!max || !min || min < 1) {
+        return 'ERROR'
+    }
+    return geometric(max) - geometric(min - 1)
+}
 
+const geometric = function(x) {
+    return (x * (x+1)) / 2
+}
+
+const isNumber = function(x) {
+    return typeof x === 'number' && isFinite(x)
 }
 
 module.exports = sumAll
