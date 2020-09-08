4734f9ea3998f9f0b8931d53e2f6ef81b0ece85a
Chris Lormor <clormor@palantir.com>
2020-09-08 22:21:29 +0100

    temperature conversion solution

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(farenheitTemp) {
+    let result = (+farenheitTemp - 32) * 5 / 9
+    return formatNumber(result)
 }
 
-const ctof = function() {
+const ctof = function(celsisusTemp) {
+    let result = +celsisusTemp * 9 / 5 + 32
+    return formatNumber(result)
+}
 
+const formatNumber = function(number) {
+    if (Number.isInteger(number)) {
+        return number
+    } else {
+        return +number.toFixed(1)
+    }
 }
 
 module.exports = {
