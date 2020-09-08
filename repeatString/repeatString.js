322e0dc4366e515309928d73775b1f940ad03058
Chris Lormor <clormor@palantir.com>
2020-09-08 17:09:16 +0100

    add solution to repeat string

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(input, times) {
+    let result = ''
+    let i = +times
+    if (times < 0) return 'ERROR'
+    for (; i > 0; i--) {
+        result += input
+    }
+    return result
 }
 
 module.exports = repeatString
