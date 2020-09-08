f481034975db23810747cad8ad48ea8aa9fe99b5
Chris Lormor <clormor@palantir.com>
2020-09-08 22:08:22 +0100

    leap year solution

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let parsedYear = parseInt(year)
+    if (parsedYear % 400 === 0) {
+        return true
+    } else {
+        if (parsedYear % 100 === 0) {
+            return false
+        } else {
+            return (parsedYear % 4 === 0)
+        }
+    }
 }
 
 module.exports = leapYears
