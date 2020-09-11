e01d5d2eb960d08612218ddcb9e29a3a168480f6
Chris Lormor <clormor@palantir.com>
2020-09-11 08:58:01 +0100

    add some more solutions and format

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-let findTheOldest = function() {
-
+let findTheOldest = function (people) {
+    return people.sort(ageSort)[0]
 }
+
+const ageSort = (p1, p2) => findAge(p2) - findAge(p1)
+const findAge = person => (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth
 
 module.exports = findTheOldest
